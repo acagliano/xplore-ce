ab942f0042f256beaf54771edbc755e2e0263224
Anthony Cagliano <acagliano97@gmail.com>
2022-02-21 10:31:48 -0500

    add and commit all files

---
+++ src/gfx/wall.h
@@ -0,0 +1,18 @@
+#ifndef wall_include_file
+#define wall_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define wall_width 8
+#define wall_height 9
+#define wall_size 74
+#define wall ((gfx_sprite_t*)wall_data)
+extern unsigned char wall_data[74];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
