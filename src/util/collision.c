1a194825ea219cf87173e664232e466359c2c3ad
Anthony Cagliano <acagliano97@gmail.com>
2022-03-05 22:22:19 -0500

    forgot to do a thing

---
+++ src/util/collision.c
@@ -0,0 +1,37 @@
+#ifndef H_COLLISION
+#define H_COLLISION
+
+#include <stdbool.h>
+#include <cstddef>
+#include <cstdint>
+#include <tice.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <string.h>
+
+#include "../util/trig.h"
+#include "../level.h"
+
+struct line_seg {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+};
+
+void process_collisions();
+
+uint8_t raycast(uint startX, uint startY, angle_t angle, const tile_t tiles[][18], struct line_seg *result);
+
+bool check_tile_collision(uint x, uint y, bool respect_holes);
+
+bool seg_collides_seg(struct line_seg *l1, struct line_seg *l2);
+bool seg_collides_seg(struct line_seg *l1, struct line_seg *l2, int *intercept_x, int *intercept_y);
+
+int y_intercept(struct line_seg *line, int x_pos);
+
+int x_intercept(struct line_seg *line, int y_pos);
+
+#endif /* H_COLLISION */
---
+++ src/util/util.h
@@ -0,0 +1,65 @@
+#include <stdbool.h>
+#include <cstddef>
+#include <cstdint>
+#include <tice.h>
+
+#ifndef H_UTIL
+#define H_UTIL
+
+// Since "unsigned int" is far too long
+typedef unsigned int uint;
+
+#define INT_BITS (sizeof(int) * 8)
+
+#define print_line() dbg_printf("%s:%u\n", __FILE__, __LINE__)
+
+enum {
+    AXIS_X,
+    AXIS_Y
+};
+
+void init_timer();
+
+void limit_framerate();
+
+// Division-by-repeated-subtraction
+// Faster than regular division when the result is less than about 150
+// About 10x faster than regular division when the result is 1, and 5x faster when the result is 20
+inline uint8_t fast_div(uint24_t num, uint24_t den) {
+    uint8_t result;
+    asm("xor\ta,a\n"
+        "loop%=:\n"
+        "\tinc\ta\n"
+        "\tsbc\thl,de\n"
+        "\tjq\tnc,loop%=\n"
+        "\tdec\ta\n"
+    : "=a" (result)
+    : "l" (num), "e" (den)
+    : "cc");
+    return result;
+}
+
+inline uint8_t div256_8(uint24_t num) {
+    uint8_t result;
+    asm(""
+    : "+b,+d,+h,+ixh,+iyh" (result)
+    : "c,e,l,ixl,iyl" (num):);
+    return result;
+}
+
+extern uint8_t div256_24_buf[4];
+inline uint24_t div256_24(uint24_t num) {
+    uint24_t result;
+    asm("ld\t(%2),hl\n"
+        "\tld\thl,(%2 + 1)"
+    : "+l" (result)
+    : "l" (num), "iyl" (div256_24_buf)
+    :);
+    return result;
+}
+
+inline uint min(uint a, uint b) { return (a > b) ? b : a; }
+
+inline uint max(uint a, uint b) { return (a > b) ? a : b; }
+
+#endif
